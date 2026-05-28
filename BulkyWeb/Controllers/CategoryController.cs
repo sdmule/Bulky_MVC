171a4a7455b1367d7f6db0362880b4a901ac3fec
sdmule <mulesaurabh45@gmail.com>
2026-05-28 10:25:57 +0530

    Added Category controller

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        
+        {
+            return View();
+        }
+    }
+}
