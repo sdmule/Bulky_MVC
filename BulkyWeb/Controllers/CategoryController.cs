a45401cd9ce1bcd5ad8eaf54585ab56fed168847
sdmule <mulesaurabh45@gmail.com>
2026-05-29 15:09:31 +0530

    Get categories to edit

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -17,6 +17,7 @@ namespace BulkyWeb.Controllers
             return View(objCategoryList);
         }
 
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
@@ -24,6 +25,45 @@ namespace BulkyWeb.Controllers
 
         [HttpPost]
         public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            //if (obj.Name.ToLower() == "test")
+            //{
+            //    ModelState.AddModelError("", "Test is an invalid value");
+            //}
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(c => c.Id == id);
+            //Category? categoryFromDb2 = _db.Categories.Where(c => c.Id == id).FirstOrDefault();
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
         {
             if (obj.Name == obj.DisplayOrder.ToString())
             {
